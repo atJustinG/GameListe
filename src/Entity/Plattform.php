b6d656a798f958f5505a2dce29bff86ed800303e
atJustinG <justinglowa@live.de>
2018-12-13 12:53:23 +0100

    AddGame Plattform integriert, bestätigung erstellt

--- src/Entity/Plattform.php
+++ src/Entity/Plattform.php
@@ -38,6 +38,11 @@ class Plattform
         $this->title = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
